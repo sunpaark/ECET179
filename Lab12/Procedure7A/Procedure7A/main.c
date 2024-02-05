60c5213f68b7168f0510ca442b5bd33b759f7204
sunpaark <sunpark8181@gmail.com>
2024-02-05 17:21:14 -0500

    Add files via upload

---
+++ Lab12/Procedure7A/Procedure7A/main.c
@@ -0,0 +1,128 @@
+/*
+ * Procedure7A.c
+ *
+ * Created: 11/10/2023 12:07:48 PM
+ * Author : park1432
+ */ 
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>		// gives us access to the delay library funcitons
+#include "serial.c"
+#include "ADC_student.c"
+
+void init_ADC_for_10bits(void)
+{
+	// setup the Analog to Digital Converter
+	ADMUX = 0x40;		// start by selecting the voltage reference - Avcc
+	ADMUX = ADMUX | 0x00;   // Select the default ADC channel - channel 0 in this case
+	ADMUX = ADMUX & ~0x20;	// set for Right Justified
+	
+	ADCSRA = 0x07;	// select the ADC clock frequency - Clock / 128
+	ADCSRA = ADCSRA | 0x80;	// enable the ADC
+
+}
+
+uint16_t read_10bit_ADC(void)
+{
+	uint8_t upper, lower;
+	uint16_t value;			// 16-bit value to hold the result
+	
+	ADMUX = ADMUX & 0xE0;		// clear the channel data
+
+	ADCSRA = ADCSRA | 0x40;	// start a conversion
+	
+	while( (ADCSRA & 0x10) == 0 )	// wait for conversion to be completed
+	{
+	}
+	
+	lower = ADCL;		// get the lower 8-bits of the conversion
+	upper = ADCH;		// get the upper 2 bits
+	
+	value = (upper << 8) | lower;		// combine the upper and lower bits here to form a 10-bit number
+	
+	ADCSRA = ADCSRA | 0x10;	// clear the conversion flag
+	
+	return value;			// send back the 8-bit result
+}
+
+
+uint16_t read_10bit_ADC_channel(uint8_t channel)
+{
+	uint8_t upper, lower;
+	uint16_t value;			// 16-bit value to hold the result
+	
+	ADMUX = ADMUX & 0xE0;		// clear the channel data
+	
+	ADMUX = ADMUX | channel;	// add the channel data here!
+
+	ADCSRA = ADCSRA | 0x40;	// start a conversion
+	
+	while( (ADCSRA & 0x10) == 0 )	// wait for conversion to be completed
+	{
+	}
+
+	lower = ADCL;		// get the lower 8-bits of the conversion
+	upper = ADCH;		// get the upper 2 bits
+	
+	value = (upper << 8) | lower;;		// combine the upper and lower bits here to form a 10-bit number
+
+	ADCSRA = ADCSRA | 0x10;	// clear the conversion flag
+	
+	return value;			// send back the 10-bit result
+}
+
+int main(void)
+{
+	DDRE = 0xFF;	// initialize OCR3A as output
+	DDRA = 0x00;	// configure Port A for input
+	PORTA = 0xFF;	// turn on the Port A pull-up resistors
+
+	init_UART();		// initialize the UART for 9600, N, 8,
+	init_ADC();
+	uint16_t pulse_width = 0;
+	uint16_t value;
+	uint16_t value2;
+	
+	TCCR3A = 0x02;		// fast PWM
+	TCCR3B = 0x18;		// fast PWM
+
+	TCCR3A = TCCR3A | 0xA0;		// set on match
+	TCCR3B = TCCR3B | 0x02;
+	
+	ICR3 = 40000;		// set frequency to 256 Hz from clock prescale
+
+	OCR3A = 1500 * 2;		// Timer 3 is initialized to 1500 uSec
+
+	while(1)
+	{
+		if (PINA == 0)
+		{
+			value = 4*read_ADC_channel(0);
+			value2 = 4*read_ADC_channel(1);
+			
+			value = value + 1000;
+			value2 = value2 + 1000;
+			
+			OCR3A = value;
+			OCR3A = value;
+			
+			printf("%u\n\r", OCR3A);
+		}
+		
+		if (PINA != 0)
+		{
+			for (value = 1000; value < 2000; value++)
+			{
+				OCR3A = value;
+				_delay_ms(10);
+			}
+
+			for (value <= 2000; value > 1000; value--)
+			{
+				OCR3A = value;
+				_delay_ms(10);
+			}
+		}
+	}
+}
