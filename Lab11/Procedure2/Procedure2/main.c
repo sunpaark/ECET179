5a77035d3af7dbed9fbc4d81d7147813d12e4260
sunpaark <sunpark8181@gmail.com>
2024-02-05 17:17:31 -0500

    Add files via upload

---
+++ Lab11/Procedure2/Procedure2/main.c
@@ -0,0 +1,39 @@
+/*
+ * Procedure2.c
+ *
+ * Created: 11/1/2023 4:26:54 PM
+ * Author : park1432
+ */ 
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>		// gives us access to the delay library funcitons
+#include "serial.c"
+#include "ADC_student.c"
+
+int main(void)
+{
+	DDRE = 0xFF;	// initialize OCR0A as output
+	
+	/*DDRC = 0x00;	// start with the port low
+	PORTC = 0xFF;	// setup Port C for input*/
+
+	init_UART();		// initialize the UART for 9600, N, 8, 1
+	init_ADC();
+
+	TCCR3A = 0x01;		// fast PWM
+	TCCR3B = 0x08;		// fast PWM
+
+	TCCR3A = TCCR3A | 0x80;		// set on match
+	TCCR3B = TCCR3B | 0x04;		// set frequency to 256 Hz from clock prescale
+
+	OCR3A = 128;		// Set 50% duty cycle
+
+	while(1)
+	{
+		OCR3A = read_ADC();		// reads the latest ADC channel
+		printf("Latest ADC value is = %u \r\n", OCR3A);	// displays the latest ADC result
+		_delay_ms(100);			// delay for 100ms
+	
+	}
+}
